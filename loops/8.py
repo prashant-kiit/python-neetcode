b333b767a3246a6b6df7b19b405fa5d7cfcfb069
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-01 22:05:06 +0530

    doc: add loops lessons

---
+++ loops/8.py
@@ -0,0 +1,44 @@
+# for loop
+
+for n in range(5):
+    print(n)
+
+print('---------')
+
+for n in range(5, 10):
+    print(n)
+
+print('---------')
+
+for n in range(5, 10, 2):
+    print(n)
+
+print('---------')
+
+for n in range(5, 10, 10):
+    print(n)
+
+print('---------')
+
+for n in range(5, 10, -1):
+    print(n)
+
+print('---------')
+
+for n in range(10, 5, -2):
+    print(n)
+
+print('---------')
+
+for n in range(10, 5):
+    print(n)
+
+print('---------')
+
+for n in range(10, 5, 2):
+    print(n)
+
+print('---------')
+
+for n in range(5, 5):
+    print(n)
