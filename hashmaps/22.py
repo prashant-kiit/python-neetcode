7608b5db933a4f0ca7d973cb95cb322daa947799
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-03 09:07:18 +0530

    doc: add dict lessons

---
+++ hashmaps/22.py
@@ -0,0 +1,17 @@
+#  dict or hashmaps
+map1 = {
+    'a' : 1,
+    'b' : 2
+}
+
+map1['c'] = 3
+
+print(map1['b'])
+
+print('a' in map1)
+
+print(len(map1))
+
+map1['c'] = 30
+
+print(map1)
---
+++ hashmaps/23.py
@@ -0,0 +1,18 @@
+# loops on dict
+
+map = {
+    'A' : 1,
+    'B' : 2
+}
+
+for key in map:
+    print(key)
+
+for value in map.values():
+    print(value)
+
+for item in map.items():
+    print(item, item[0], item[1])
+    key, value = item
+    print(key, value)
+
