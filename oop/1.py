62574e28e1f79efd2033d8ff5688c3e94ed7db2a
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-04 00:10:12 +0530

    doc: add oop lesson

---
+++ oop/1.py
@@ -0,0 +1,25 @@
+class Car:
+    def __init__(self, mileage, speed):
+        self.mileage = mileage
+        self.speed = speed
+    
+    def getMileage(self):
+        return self.mileage
+    
+    def getSpeed(self):
+        return self.speed
+
+    def setMileage(self, mileage):
+        self.mileage = mileage
+
+    def setSpeed(self, speed):
+        self.speed = speed
+
+def main():
+    car = Car(50, 100)
+    car.setMileage(60)
+    print(car.getMileage())
+
+if __name__ == "__main__":
+    main()
+        
