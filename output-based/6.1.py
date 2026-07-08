09be28536b08ccdfea5eac4c3fd94776dd05180d
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-08 20:32:04 +0530

    feat: add example and documentation for multiple inheritance and Method Resolution Order (MRO) in Python

---
+++ output-based/6.1.py
@@ -0,0 +1,17 @@
+class A:
+    def who(self):
+        return "A"
+
+class B(A):
+    def who(self):
+        return "B"
+
+class C(A):
+    def who(self):
+        return "C"
+
+class D(B, C):
+    pass
+
+print(D().who())
+print([c.__name__ for c in D.__mro__])
