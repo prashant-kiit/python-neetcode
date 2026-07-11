906c0a4c86ff4eaf5bb142dacda74dff91dd2ea7
prashant-m4 <prashantsingh090798@gmail.com>
2026-07-11 23:57:48 +0530

    feat: add string checking examples demonstrating various string methods in string-checkings.py

---
+++ interview-bit/string-checkings.py
@@ -0,0 +1,9 @@
+s = "Hello123"
+
+s.isalnum()      # True
+s.isalpha()      # False
+s.isdigit()      # False
+s.islower()      # False
+s.isupper()      # False
+s.isascii()      # True
+s.isidentifier() # True
